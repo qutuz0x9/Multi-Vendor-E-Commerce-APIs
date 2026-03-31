37eaf8135664ce019b4b9d9690ad728b631c0b81
0xisagi9 <muhammed9alii9@gmail.com>
2026-03-31 23:09:05 +0200

    feat(context): Initial DbContext.

---
+++ src/MultiVendorECommerce.Infrastructure/Contexts/ECommerceDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MultiVendorECommerce.Domain.Enums;
+using MultiVendorECommerce.Domain.Models;
+
+namespace MultiVendorECommerce.Infrastructure.Contexts;
+
+public class ECommerceDbContext : IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
+{
+    public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.HasPostgresEnum<UserStatus>("user_status");
+
+        builder.ApplyConfigurationsFromAssembly(typeof(ECommerceDbContext).Assembly);
+    }
+
+}
