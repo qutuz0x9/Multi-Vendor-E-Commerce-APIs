01a0a9dd5d5caa6204370abf1f3e268e1e32c2db
0xisagi9 <muhammed9alii9@gmail.com>
2026-03-31 23:09:39 +0200

    feat(dependency): Implement Infrastructure Services.

---
+++ src/MultiVendorECommerce.API/Configurations/InfrastructureConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiVendorECommerce.Domain.Enums;
+using MultiVendorECommerce.Infrastructure.Contexts;
+
+namespace MultiVendorECommerce.API.Configurations;
+
+public static class InfrastructureConfigurations
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add infrastructure services here (e.g., database context, repositories, etc.)
+
+        services.AddDbContext<ECommerceDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection"),
+                npgsqlOptions =>
+                {
+                    npgsqlOptions.MigrationsAssembly(typeof(ECommerceDbContext).Assembly.FullName);
+                    npgsqlOptions.MapEnum<UserStatus>("user_status");
+                }));
+
+        return services;
+    }
+}
---
+++ src/MultiVendorECommerce.API/Program.cs
@@ -0,0 +1,32 @@
+using MultiVendorECommerce.API.Configurations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Load Environment Specific Configuration
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+
+
+
+// Add services to the container.
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
