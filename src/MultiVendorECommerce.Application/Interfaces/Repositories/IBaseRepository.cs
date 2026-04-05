50a57457c6714373b7a156b7b98d0d85a6895d7a
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-05 22:23:18 +0200

    feat(repo): Implement Base Repository.
    Base Repository is the Parent Repo for all domain model that contain most common methods that any domain model need.

---
+++ src/MultiVendorECommerce.Application/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace MultiVendorECommerce.Application.Interfaces.Repositories;
+
+public interface IBaseRepository<TEntity, TKey> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(TKey id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<TEntity> AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+    Task<int> CountAsync(Expression<Func<TEntity, bool>>? predicate = null);
+    Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+}
---
+++ src/MultiVendorECommerce.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq.Expressions;
+using MultiVendorECommerce.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.Replication;
+using MultiVendorECommerce.Application.Interfaces.Repositories;
+
+namespace MultiVendorECommerce.Infrastructure.Repositories;
+
+public class BaseRepository<TEntity, TKey> : IBaseRepository<TEntity, TKey>
+    where TEntity : class
+{
+    protected readonly ECommerceDbContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+
+    public BaseRepository(ECommerceDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<TEntity>();
+    }
+
+    public virtual async Task<TEntity?> GetByIdAsync(TKey id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public virtual async Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _dbSet.FirstOrDefaultAsync(predicate);
+    }
+
+    public virtual async Task<TEntity> AddAsync(TEntity entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public virtual async Task UpdateAsync(TEntity entity)
+    {
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteAsync(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public virtual async Task<int> CountAsync(Expression<Func<TEntity, bool>>? predicate = null)
+    {
+        return predicate == null ? await _dbSet.CountAsync() : await _dbSet.CountAsync(predicate);
+    }
+
+    public virtual async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _dbSet.AnyAsync(predicate);
+    }
+}
