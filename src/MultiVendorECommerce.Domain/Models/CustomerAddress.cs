406266e9db5a88381c66b6c79eb508ee82f8d7a1
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-05 19:57:15 +0200

    feat(customer): Add Customer Address Domain Model ((Forget to commit it with previous commit))

---
+++ src/MultiVendorECommerce.Domain/Models/CustomerAddress.cs
@@ -0,0 +1,21 @@
+﻿using MultiVendorECommerce.Domain.Enums;
+using Npgsql.Replication;
+
+namespace MultiVendorECommerce.Domain.Models;
+
+public class CustomerAddress
+{
+    public int Id { get; set; }
+    public Guid CustomerId { get; set; }
+    public string Address { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string Country { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public CustomerAddressType AddressType { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+
+    // Navigation properties
+    public Customer Customer { get; set; } = null!;
+
+}
