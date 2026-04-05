57cd28ccf5c3bafd65ca341e72c84dcfd84b6375
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-05 22:29:12 +0200

    feat(context): Implement DbSets in Context

--- src/MultiVendorECommerce.Infrastructure/Contexts/ECommerceDbContext.cs
+++ src/MultiVendorECommerce.Infrastructure/Contexts/ECommerceDbContext.cs
@@ -13,10 +13,28 @@ public class ECommerceDbContext : IdentityDbContext<User, Role, Guid, UserClaim,
     {
     }
 
+    // Domain Models DbSets
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<Brand> Brands { get; set; } = null!;
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<ProductCategory> ProductCategories { get; set; } = null!;
+    public DbSet<Vendor> Vendors { get; set; } = null!;
+    public DbSet<VendorOffer> VendorOffers { get; set; } = null!;
+    public DbSet<Inventory> Inventories { get; set; } = null!;
+    public DbSet<Customer> Customers { get; set; } = null!;
+    public DbSet<CustomerAddress> CustomerAddresses { get; set; } = null!;
+    public DbSet<VendorAddress> VendorAddresses { get; set; } = null!;
+    public DbSet<CartSession> CartSessions { get; set; } = null!;
+    public DbSet<CartItem> CartItems { get; set; } = null!;
+    public DbSet<Order> Orders { get; set; } = null!;
+    public DbSet<OrderItem> OrderItems { get; set; } = null!;
+    public DbSet<Payment> Payments { get; set; } = null!;
+    public DbSet<OrderShippingAddress> OrderShippingAddresses { get; set; } = null!;
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        
+
 
         builder.ApplyConfigurationsFromAssembly(typeof(ECommerceDbContext).Assembly);
     }
