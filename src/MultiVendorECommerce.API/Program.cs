2f9323eeb2a59cd7c1243e9fece4482543a39a0c
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-06 22:18:43 +0200

    update(program): Register services, middlewares.

--- src/MultiVendorECommerce.API/Program.cs
+++ src/MultiVendorECommerce.API/Program.cs
@@ -1,4 +1,7 @@
 using MultiVendorECommerce.API.Configurations;
+using MultiVendorECommerce.API.Extensions;
+using Serilog;
+using MultiVendorECommerce.API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,13 +12,16 @@ builder.Configuration
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
-
+// Configure Serilog
+SerilogConfigurations.ConfigureSerilog(builder);
 
 
 // Add services to the container.
-builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddInfrastructureServices(builder.Configuration); // Add infrastructure services (e.g., database context, repositories, etc.)
+builder.Services.AddLoggingServices(); // Add logging services (e.g., Serilog, NLog, etc.)
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
@@ -25,8 +31,36 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+
+// Add Middlewares
+app.UseMiddleware<GlobalExceptionHandlerMiddleware>(); // Custom middleware for global exception handling
+app.UseMiddleware<RequestLoggingMiddleware>(); // Custom middleware for logging HTTP requests and responses
+
+// Serilog request logging with enriched context
+app.UseSerilogRequestLogging(opts =>
+{
+    opts.MessageTemplate =
+        "HTTP {RequestMethod} {RequestPath} : {StatusCode} ({Elapsed:0.0000}ms)";
+    opts.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+    {
+        diagnosticContext.Set("RemoteIP", httpContext.Connection.RemoteIpAddress);
+        diagnosticContext.Set("UserAgent", httpContext.Request.Headers.UserAgent.ToString());
+    };
+});
+
+
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.Run();
+try
+{
+    Log.Information("Starting MultiVendor E-Commerce API");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush(); // ensures all buffered logs are flushed on shutdown
+}
