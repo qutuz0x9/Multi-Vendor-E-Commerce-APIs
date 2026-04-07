2e7a4af97277bad5fe99a27a6310142396b71f85
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-07 19:24:11 +0200

    feat(result): Implement Result Pattern.
    The Result Pattern is a way to return outcomes from your methods without relying on exceptions for normal control flow
    - Implement Result Class for normal responses.
    - Implement Generic Result Class for specific DTOs.

---
+++ src/MultiVendorECommerce.Shared/Results/Result.cs
@@ -0,0 +1,61 @@
+﻿using MultiVendorECommerce.Shared.Helpers;
+using System.Runtime;
+
+namespace MultiVendorECommerce.Shared.Results;
+
+/// <summary>
+/// Represents the result of an operation, which can be either a success or a failure with associated errors.
+/// </summary>
+public class Result
+{
+  
+    protected readonly List<Error> _errors = new();
+    public int StatusCode { get; set; } = 200;
+    public bool IsSuccess => !_errors.Any();
+    public bool IsFailure => _errors.Any();
+    public IReadOnlyList<Error> Errors => _errors.AsReadOnly();
+
+    protected Result() { }
+    protected Result(int statusCode)
+    {
+        StatusCode = statusCode;
+    }
+    protected Result(Error error, int statusCode)
+    {
+        _errors.Add(error);
+        StatusCode = statusCode;
+    }
+    protected Result(IEnumerable<Error> errors, int statusCode)
+    {
+        _errors.AddRange(errors);
+        StatusCode = statusCode;
+    }
+
+    public static Result Success() => new();
+    public static Result Success(int statusCode = 200) => new(statusCode);
+    public static Result Failure(Error error, int statusCode = 404) => new(error, statusCode);
+    public static Result Failure(IEnumerable<Error> errors, int statusCode = 404) => new(errors, statusCode);
+}
+
+
+/// <summary>
+/// Represents the result of an operation that can either be a success with a value of type T or a failure with associated errors.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Result<T> : Result
+{
+    public T? Value { get; }
+    private Result(T value)
+    {
+        Value = value;
+    }
+    private Result(T value, int statusCode) : base(statusCode)
+    {
+        Value = value;
+    }
+    private Result(Error error, int statusCode) : base(error, statusCode) { }
+    private Result(IEnumerable<Error> errors, int statusCode) : base(errors, statusCode) { }
+    public static Result<T> Success(T value, int statusCode = 200) => new(value, statusCode);
+    public static new Result<T> Failure(Error error, int statusCode = 404) => new(error, statusCode);
+    public static new Result<T> Failure(IEnumerable<Error> errors, int statusCode = 404) => new(errors, statusCode);
+}
