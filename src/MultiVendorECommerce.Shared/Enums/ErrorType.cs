bfb91391179d19cd69f64b1e7a7df8a5e7d2c6c2
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-07 19:22:58 +0200

    feat(error): Implement Error class to help me in implementation of Result Pattern.

---
+++ src/MultiVendorECommerce.Shared/Enums/ErrorType.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiVendorECommerce.Shared.Enums;
+
+public enum ErrorType
+{
+
+    Failure = 0,
+    Validation = 1,
+    NotFound = 2,
+    Unauthorized = 3,
+    Forbidden = 4,
+    InvalidCredentials = 5
+}
---
+++ src/MultiVendorECommerce.Shared/Helpers/Error.cs
@@ -0,0 +1,30 @@
+﻿using MultiVendorECommerce.Shared.Enums;
+
+namespace MultiVendorECommerce.Shared.Helpers;
+
+public class Error
+{
+
+    public string ErrorMessage { get; }
+    public ErrorType Type { get; }
+
+    private Error(string errorMessage, ErrorType type)
+    {
+        ErrorMessage = errorMessage;
+        Type = type;
+    }
+
+    public static Error Failure(string errorMessage = "There was an error processing your request.") =>
+        new(errorMessage, ErrorType.Failure);
+    public static Error Validation(string errorMessage = "One or more validation errors occurred.") =>
+        new(errorMessage, ErrorType.Validation);
+    public static Error NotFound(string errorMessage = "The requested resource was not found.") =>
+        new(errorMessage, ErrorType.NotFound);
+    public static Error Unauthorized(string errorMessage = "You are not authorized to perform this action.") =>
+        new(errorMessage, ErrorType.Unauthorized);
+    public static Error Forbidden(string errorMessage = "You do not have permission to access this resource.") =>
+        new(errorMessage, ErrorType.Forbidden);
+    public static Error InvalidCredentials(string errorMessage = "The provided credentials are invalid.") =>
+        new(errorMessage, ErrorType.InvalidCredentials);
+
+}
