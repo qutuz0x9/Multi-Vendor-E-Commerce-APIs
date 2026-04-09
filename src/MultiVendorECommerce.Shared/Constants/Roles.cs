21b8231a4f753e4d2e1f4972258325ac11ec4f4a
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-09 23:15:33 +0200

    feat(roles): Implement constant Roles.
    - I'm working on Auth Services but i still learning how to use TDD (Test Driven Development), I understand things good but in practice it's difficult i have hard time in learning. I don't know it's a normal curve or my learning curve is so long.

---
+++ src/MultiVendorECommerce.Shared/Constants/Roles.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime;
+
+namespace MultiVendorECommerce.Shared.Constants;
+
+public static class Roles
+{
+    public const string Admin = "Admin";
+    public const string Vendor = "Vendor";
+    public const string Customer = "Customer";
+}
