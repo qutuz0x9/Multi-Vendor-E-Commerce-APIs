bff51cb67a140e1628481a532213f09129ea3fdd
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-10 22:53:18 +0200

    feat(mapper): Implement AutoMapper Profile for DTOs.

---
+++ src/MultiVendorECommerce.Application/Profiles/UserProfile.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using MultiVendorECommerce.Application.DTOs.Auth;
+using MultiVendorECommerce.Domain.Models;
+namespace MultiVendorECommerce.Application.Profiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<RegisterUserDTO, User>()
+        .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Username))
+        .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email)).ReverseMap();
+
+        CreateMap<User, RegisterResponseDTO>()
+        .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+        .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Id))
+        .ForMember(dest => dest.Role, opt => opt.Ignore())
+        .ForMember(dest => dest.Token, opt => opt.Ignore()).ReverseMap();
+
+    }
+}
