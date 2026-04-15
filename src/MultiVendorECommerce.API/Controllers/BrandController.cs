ae5ffacceedac6746b9baf106b06e0bf1326be4b
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-15 17:43:28 +0200

    feat(brand): implement BrandController with CRUD operations for brands

---
+++ src/MultiVendorECommerce.API/Controllers/BrandController.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiVendorECommerce.Application.DTOs.Brand;
+using MultiVendorECommerce.Application.Interfaces.Services;
+using MultiVendorECommerce.Shared.Results;
+
+namespace MultiVendorECommerce.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BrandController(IBrandService brandService) : ControllerBase
+{
+    protected readonly IBrandService _brandService = brandService;
+
+    [HttpGet]
+    [ProducesResponseType(typeof(Result<IEnumerable<BrandDTO>>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status403Forbidden)]
+    [Authorize(Roles = "Admin,Vendor")]
+    public async Task<ActionResult<Result<IEnumerable<BrandDTO>>>> GetAll()
+    {
+        var result = await _brandService.GetAllAsync();
+        return StatusCode(result.StatusCode, result);
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(Result<BrandDTO>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status403Forbidden)]
+    [Authorize(Roles = "Admin,Vendor")]
+    public async Task<ActionResult<Result<BrandDTO>>> GetById(int id)
+    {
+        var result = await _brandService.GetByIdAsync(id);
+        return StatusCode(result.StatusCode, result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(Result<BrandDTO>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status403Forbidden)]
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult<Result<BrandDTO>>> Create([FromBody] CreateBrandDTO request)
+    {
+        var result = await _brandService.CreateAsync(request);
+        return StatusCode(result.StatusCode, result);
+    }
+
+    [HttpPut("{id:int}")]
+    [ProducesResponseType(typeof(Result<BrandDTO>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status403Forbidden)]
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult<Result<BrandDTO>>> Update(int id, [FromBody] UpdateBrandDTO request)
+    {
+        var result = await _brandService.UpdateAsync(id, request);
+        return StatusCode(result.StatusCode, result);
+    }
+
+    [HttpDelete("{id:int}")]
+    [ProducesResponseType(typeof(Result<BrandDTO>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status403Forbidden)]
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult<Result>> Delete(int id)
+    {
+        var result = await _brandService.DeleteAsync(id);
+        return StatusCode(result.StatusCode, result);
+    }
+}
