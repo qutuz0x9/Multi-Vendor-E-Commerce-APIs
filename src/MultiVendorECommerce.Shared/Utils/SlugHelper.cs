5c25607bdd80b1db6b468387c58a9bb6a3739252
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-17 21:14:25 +0200

    feat(slug): add methods to generate product slugs and extract values from JSON features

--- src/MultiVendorECommerce.Shared/Utils/SlugHelper.cs
+++ src/MultiVendorECommerce.Shared/Utils/SlugHelper.cs
@@ -1,11 +1,64 @@
 using System.Globalization;
 using System.Text;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 
 namespace MultiVendorECommerce.Shared.Utils;
 
 public static partial class SlugHelper
 {
+    public static string GenerateProductSlug(string name, JsonDocument? features)
+    {
+        var parts = new List<string> { name };
+
+        if (features is not null)
+            ExtractJsonValues(features.RootElement, parts);
+
+        return GenerateSlug(string.Join(" ", parts));
+    }
+
+    public static string GenerateUniqueProductSlug(string name, JsonDocument? features, Func<string, bool> slugExists)
+    {
+        string baseSlug = GenerateProductSlug(name, features);
+        string slug = baseSlug;
+        int counter = 1;
+
+        while (slugExists(slug))
+        {
+            slug = $"{baseSlug}-{counter}";
+            counter++;
+        }
+
+        return slug;
+    }
+
+    private static void ExtractJsonValues(JsonElement element, List<string> values)
+    {
+        switch (element.ValueKind)
+        {
+            case JsonValueKind.Object:
+                foreach (var property in element.EnumerateObject())
+                    ExtractJsonValues(property.Value, values);
+                break;
+            case JsonValueKind.Array:
+                foreach (var item in element.EnumerateArray())
+                    ExtractJsonValues(item, values);
+                break;
+            case JsonValueKind.String:
+                var str = element.GetString();
+                if (!string.IsNullOrWhiteSpace(str))
+                    values.Add(str);
+                break;
+            case JsonValueKind.Number:
+                values.Add(element.GetRawText());
+                break;
+            case JsonValueKind.True:
+            case JsonValueKind.False:
+                values.Add(element.GetBoolean().ToString());
+                break;
+        }
+    }
+
     public static string GenerateSlug(string value)
     {
         if (string.IsNullOrWhiteSpace(value))
