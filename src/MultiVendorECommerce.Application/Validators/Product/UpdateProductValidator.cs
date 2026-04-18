cd09a30157221c44d72d1cd6184d25d93cc668e2
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-18 13:17:01 +0200

    feat(validator): add UpdateProductValidator for validating product updates

---
+++ src/MultiVendorECommerce.Application/Validators/Product/UpdateProductValidator.cs
@@ -0,0 +1,25 @@
+using FluentValidation;
+using MultiVendorECommerce.Application.DTOs.Product;
+
+namespace MultiVendorECommerce.Application.Validators.Product;
+
+public class UpdateProductValidator : AbstractValidator<UpdateProductDTO>
+{
+    public UpdateProductValidator()
+    {
+        RuleFor(x => x.BrandId)
+            .GreaterThan(0).WithMessage("A valid brand is required.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Product name is required.")
+            .MinimumLength(2).WithMessage("Product name must be at least 2 characters long.")
+            .MaximumLength(200).WithMessage("Product name must not exceed 200 characters.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Product description is required.")
+            .MaximumLength(2000).WithMessage("Product description must not exceed 2000 characters.");
+
+        RuleFor(x => x.Status)
+            .IsInEnum().WithMessage("Invalid product status.");
+    }
+}
