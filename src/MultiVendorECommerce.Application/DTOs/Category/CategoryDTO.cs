8114fb0de7b4facedfb6228bd00df51d410840eb
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-20 17:19:39 +0200

    refactor(category): remove unused properties from CategoryDTO

--- src/MultiVendorECommerce.Application/DTOs/Category/CategoryDTO.cs
+++ src/MultiVendorECommerce.Application/DTOs/Category/CategoryDTO.cs
@@ -7,8 +7,5 @@ public class CategoryDTO
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
-    public string? Slug { get; set; }
-    public CategoryStatus Status { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime? ModifiedAt { get; set; }
+
 }
