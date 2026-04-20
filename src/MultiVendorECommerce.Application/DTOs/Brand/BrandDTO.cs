7cd3820ab64872126d965323cf5c2081045233d3
0xisagi9 <muhammed9alii9@gmail.com>
2026-04-20 14:17:10 +0200

    refactor(brand): remove unused properties from BrandDTO class

--- src/MultiVendorECommerce.Application/DTOs/Brand/BrandDTO.cs
+++ src/MultiVendorECommerce.Application/DTOs/Brand/BrandDTO.cs
@@ -6,8 +6,6 @@ public class BrandDTO
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
-    public string? Slug { get; set; }
-    public BrandStatus Status { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime? ModifiedAt { get; set; }
+
+
 }
